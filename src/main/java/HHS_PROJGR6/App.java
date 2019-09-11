2c188266732f9722834f152d10e66a6c6b9fe23c
FerryGood <f.minke@student.hhs.nl>
2019-09-11 22:57:19 +0200

    added comment

--- src/main/java/HHS_PROJGR6/App.java
+++ src/main/java/HHS_PROJGR6/App.java
@@ -5,8 +5,10 @@ package HHS_PROJGR6;
 
 public class App {
     public String getGreeting() {
-        return "Hello world.";
+        return "Hello world from group 6.";
     }
+
+    // Ghallo
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
