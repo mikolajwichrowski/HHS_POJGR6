7904bc4a6908bf4a2a3be5c711a470cccf0b65dc
FerryGood <f.minke@student.hhs.nl>
2019-09-15 18:01:21 +0200

    Java Grid en buttons

--- src/main/java/HHS_PROJGR6/App.java
+++ src/main/java/HHS_PROJGR6/App.java
@@ -1,45 +1,52 @@
 package HHS_PROJGR6;
 
-import java.awt.Dimension;
-
-import javax.swing.*;  
+import javax.swing.*;
+import java.awt.*;
 
 public class App extends JFrame {
+
     /**
      * Generated serial id
      */
+
     private static final long serialVersionUID = -2280803615953081532L;
 
     public App() {
         // Set dimensions and add canvas to draw the hotel on
-        Dimension d = new Dimension(400, 500);
-        add(new Canvas(d));
+        Dimension d = new Dimension(1700, 1100);
+        Hotel myHotel = new Hotel(new Canvas(d));
+        add(myHotel.hotelCanvas);
 
         // Add buttons and add dimension to window to fit buttons
         d.width += 200;
 
-        JButton buttonPlus = new JButton("HTE +");
-        buttonPlus.setBounds(600, 0,100, 40);
+        JButton buttonPlus = new JButton("Hotel TijdsEenheid +");
+        buttonPlus.setBounds(1600, 50,200, 80);
+        buttonPlus.setBackground(Color.DARK_GRAY);
+        buttonPlus.setForeground(Color.WHITE);
 
-        JButton buttonMinus = new JButton("HTE -");
-        buttonMinus.setBounds(600, 50,100, 40);
- 
-        add(buttonMinus);
+        JButton buttonMinus = new JButton("Hotel TijdsEenheid -");
+        buttonMinus.setBounds(1600, 150,200, 80);
+        buttonMinus.setBackground(Color.DARK_GRAY);
+        buttonMinus.setForeground(Color.WHITE);
+
+        add(buttonPlus);
         add(buttonMinus);
 
         // Show window
         setTitle("HotelSimulatie GR6");
         setLayout(null);
-        setResizable(false);
+        setResizable(true);
         setSize(d);
         setLayout(null);
+        super.setLocationRelativeTo(null);
         setVisible(true);
     }
 
     public static void main(String[] args) {
         // Run Application
         // Show buttons
-        // new Canvas();    
+        // new Canvas();
         new App();
     }
 }
