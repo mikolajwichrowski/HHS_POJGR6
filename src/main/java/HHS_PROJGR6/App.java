e25175b9410291f894cfaa12888c38638773d68e
FerryGood <f.minke@student.hhs.nl>
2019-10-10 20:05:06 +0200

    Merge branch 'Fer' of https://github.com/mikolajwichrowski/HHS_PROJGR6 into Fer
    # Conflicts:
    #	src/main/java/HHS_PROJGR6/Hotel.java

--- src/main/java/HHS_PROJGR6/App.java
+++ src/main/java/HHS_PROJGR6/App.java
@@ -31,16 +31,44 @@ public class App extends JFrame {
         Dimension d = new Dimension(getPixelResolution() * (myHotel.getHighestPositions()[0] + 4), getPixelResolution() * (myHotel.getHighestPositions()[1] + 5));
         Canvas canvas = new Canvas(d);
 
+        JPanel p = new JPanel();
+        p.setBounds(10,500,200,400);
+
+        JLabel groen = new JLabel("Groen = Kamer is schoon");
+        groen.setForeground(Color.GREEN);
+        JLabel oranje = new JLabel("Oranje = Kamer wordt schoongemaakt");
+        oranje.setForeground(Color.ORANGE);
+        JLabel red = new JLabel("Rood = Kamer vies");
+        red.setForeground(Color.RED);
+        JLabel menu = new JLabel("M = menu");
+        JLabel settings = new JLabel("S = Settings");
+
+        p.add(groen);
+        p.add(oranje);
+        p.add(red);
+        p.add(menu);
+        p.add(settings);
+
+        groen.setPreferredSize(new Dimension(200,15));
+        oranje.setPreferredSize(new Dimension(200,15));
+        red.setPreferredSize(new Dimension(200,15));
+        menu.setPreferredSize(new Dimension(200,15));
+        settings.setPreferredSize(new Dimension(200,15));
+
         myHotel.setHotelCanvas(canvas);
 
         // Adding components to the JFrame
         add(myHotel.getHotelCanvas());
+        add(p);
+
+
+
 
         // Options for the JFrame
         setTitle("HotelSimulatie GR6");
         setResizable(true);
         setSize(d);
-        getContentPane().setBackground(Color.WHITE);
+        getContentPane().setBackground(new Color(245, 245, 240));
         setLayout(null);
         setLocation(350, 0);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
